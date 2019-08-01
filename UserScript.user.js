b6a2542ce63a6c58c93b52bfb200745a61922577
某亚瑟 <mouyase@qq.com>
2019-08-01 14:46:08 +0800

    Update js

--- UserScript.user.js
+++ UserScript.user.js
@@ -2,13 +2,12 @@
 // @name         Booru 图站标签汉化插件
 // @namespace    https://github.com/4cy/Script-BooruTagCN
 // @version      0.3.3
-// @description  Booru 图站标签汉化插件
+// @description  Booru 图站标签汉化插件，用于汉化各种图库网站的标签
 // @author       某亚瑟
 // @match        https://yande.re/*
 // @match        https://gelbooru.com/*
 // @match        https://konachan.com/*
 // @match        https://konachan.net/*
-// @grant        none
 // @require      https://code.jquery.com/jquery-3.3.1.js
 // ==/UserScript==
 (function() {
@@ -21,7 +20,7 @@
 		jQuery("ul#tag-sidebar li a").each(function(){
 			var obj = jQuery(this);
 				if (KeyMap.hasOwnProperty(obj.html())) {
-					obj.html(KeyMap[obj.html()]+"【"+obj.html()+"】");
+					obj.html(KeyMap[obj.html()]+"["+obj.html()+"]");
 				}
 		});
 		console.log('TAG汉化已完成');
