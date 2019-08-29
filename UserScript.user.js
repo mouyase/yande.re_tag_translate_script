4c4835a5cbfb5d9096157cd84098a939e09d95c3
某亚瑟 <mouyase@qq.com>
2019-08-30 00:27:01 +0800

    Update UserScript.user.js

--- UserScript.user.js
+++ UserScript.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Booru 图站标签汉化插件
 // @namespace    https://github.com/mouyase/Script-BooruTagCN
-// @version      0.3.5
+// @version      0.3.6
 // @description  Booru 图站标签汉化插件，用于汉化各种图库网站的标签
 // @author       某亚瑟
 // @match        https://yande.re/*
@@ -10,23 +10,34 @@
 // @match        https://konachan.net/*
 // ==/UserScript==
 (function() {
-	'use strict';
+    'use strict';
 
-	function modify() {
-		jQuery("ul#tag-sidebar li a").each(function(){
-			var obj = jQuery(this);
-			var tag = decodeURIComponent(obj.attr('href')).split("tags=")[1];
-			if(tag){
-				if (tag.indexOf("-")==-1&&tag.indexOf("+")==-1&&tag.indexOf("?")==-1) {
-					var url = 'https://cdn.jsdelivr.net/gh/mouyase/BooruTags-CN@gh-pages/'+window.btoa(tag);
-					jQuery.get(url,function(translate){
-						obj.html(translate);
-					});
-				}
-			}
-		});
-		console.log('TAG汉化已完成');
-	}
-
-	modify();
+    function modify() {
+        jQuery("ul#tag-sidebar li a").each(function(){
+            var obj = jQuery(this);
+            var tag = decodeURIComponent(obj.attr('href')).split("tags=")[1];
+            if(tag){
+                if (tag.indexOf("-")==-1&&tag.indexOf("+")==-1&&tag.indexOf("?")==-1) {
+                    var url = 'https://cdn.jsdelivr.net/gh/mouyase/BooruTags-CN@gh-pages/'+window.btoa(tag);
+                    jQuery.get(url,function(translate){
+                        obj.html(translate);
+                    });
+                }
+            }
+        });
+        jQuery("tr.even a").each(function(){
+            var obj = jQuery(this);
+            var tag = decodeURIComponent(obj.attr('href')).split("tags=")[1];
+            if(tag){
+                if (tag.indexOf("-")==-1&&tag.indexOf("+")==-1&&tag.indexOf("?")==-1) {
+                    var url = 'https://cdn.jsdelivr.net/gh/mouyase/BooruTags-CN@gh-pages/'+window.btoa(tag);
+                    jQuery.get(url,function(translate){
+                        obj.html(translate);
+                    });
+                }
+            }
+        });
+        console.log('TAG汉化已完成');
+    }
+    modify();
 })();
